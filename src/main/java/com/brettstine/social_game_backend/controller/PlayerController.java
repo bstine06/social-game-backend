1d4f159822a6c9531ed751464bfece51cccbccab
Brett Stine <r.brettstine@gmail.com>
2024-09-16 22:06:53 -0400

    Exception handling and logging in PlayerController

--- src/main/java/com/brettstine/social_game_backend/controller/PlayerController.java
+++ src/main/java/com/brettstine/social_game_backend/controller/PlayerController.java
@@ -1,5 +1,6 @@
 package com.brettstine.social_game_backend.controller;
 
+import com.brettstine.social_game_backend.model.GameState;
 import com.brettstine.social_game_backend.model.PlayerModel;
 import com.brettstine.social_game_backend.service.GameFlowService;
 import com.brettstine.social_game_backend.service.PlayerService;
@@ -9,6 +10,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -41,17 +43,16 @@ public class PlayerController {
     @PostMapping("/create-player")
     public ResponseEntity<?> createPlayer(HttpServletResponse response, @RequestBody Map<String, String> payload) {
         String name = payload.get("name");
-        if (name == null) {
-            return ResponseEntity.status(400).body(Map.of("error", "no name provided"));
-        }
         String gameId = payload.get("gameId");
-        if (gameId == null) {
-            return ResponseEntity.status(400).body(Map.of("error", "no gameId provided"));
-        }
 
         try {
+            gameFlowService.validateGame(gameId);
+            gameFlowService.ensureGameState(gameId, GameState.LOBBY);
+
             PlayerModel player = playerService.createPlayer(gameId, name);
             String playerId = player.getPlayerId();
+            logger.info("Game: {} : Player created with ID: {}, name: {}", gameId, playerId, name);
+
             Cookie cookie = new Cookie("playerId", playerId);
             cookie.setHttpOnly(true);
             cookie.setPath("/");
@@ -60,61 +61,66 @@ public class PlayerController {
             response.addCookie(cookie);
             logger.info("Player cookie set with ID: {}", playerId);
 
-            // Create a JSON response with player information
             return ResponseEntity.ok(player);
+        } catch (IllegalArgumentException e) {
+            logger.error("Error creating player", e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "could not create player", "message", e.getMessage()));
+        } catch (IllegalStateException e) {
+            logger.error("Error creating player", e);
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("error", "could not create player", "message", e.getMessage()));
         } catch (Exception e) {
-            return ResponseEntity.status(500).body(Map.of("error", "could not create player", "message", e.getMessage()));
+            logger.error("Error creating player", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "could not create player", "message", e.getMessage()));
         }
-        
     }
 
     @GetMapping("/get-player")
     public ResponseEntity<?> getPlayer(HttpServletRequest request) {
         String playerId = CookieUtil.getDataFromCookie(request, "playerId");
-        if (playerId == null) {
-            return ResponseEntity.status(400).body(Map.of("error", "No playerId found"));
-        }
-        
         try {
             PlayerModel player = playerService.getPlayer(playerId);
+            logger.info("Successfully retrieved player with id: {}", playerId);
             return ResponseEntity.ok(player);
+        } catch (IllegalArgumentException e) {
+            logger.error("Error retrieving player with id: {}", playerId, e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "could not set player", "message", e.getMessage()));
         } catch (Exception e) {
-            return ResponseEntity.status(500).body(Map.of("error", "could not set player", "message", e.getMessage()));
+            logger.error("Error retrieving player with id: {}", playerId, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "could not set player", "message", e.getMessage()));
         }
     }
 
     @PostMapping("/set-name")
     public ResponseEntity<?> setName(HttpServletRequest request, @RequestBody Map<String, String> payload) {
         String name = payload.get("name");
-        if (name == null) {
-            return ResponseEntity.status(400).body(Map.of("error", "no name provided"));
-        }
         String playerId = CookieUtil.getDataFromCookie(request, "playerId");
-        if (playerId == null) {
-            return ResponseEntity.status(400).body(Map.of("error", "No playerId found"));
-        }
-
         try {
             PlayerModel player = playerService.setName(playerId, name);
+            logger.info("Successfully set name for player with id: {}, name: {}", playerId, name);
             return ResponseEntity.ok(player);
+        } catch (IllegalArgumentException e) {
+            logger.error("Error setting name of player with id: {}", playerId, e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "could not set player", "message", e.getMessage()));
         } catch (Exception e) {
-            return ResponseEntity.status(500).body(Map.of("error", "could not set player", "message", e.getMessage()));
+            logger.error("Error setting name of player with id: {}", playerId, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "could not set player", "message", e.getMessage()));
         }
     }
 
     @GetMapping("/get-all-players-in-game")
     public ResponseEntity<?> getAllPlayersInGame(@RequestBody Map<String, String> payload) {
         String gameId = payload.get("gameId");
-        if (gameId == null) {
-            return ResponseEntity.status(400).body(Map.of("error", "no gameId provided"));
-        }
-
         try {
             gameFlowService.validateGame(gameId);
             List<PlayerModel> allPlayers = playerService.getAllPlayersByGameId(gameId);
+            logger.info("Game: {} : Successfully retrieved all players", gameId);
             return ResponseEntity.ok(allPlayers);
+        } catch (IllegalArgumentException e) {
+            logger.error("Game: {} : Error retrieving all players", gameId, e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "could not get players", "message", e.getMessage()));
         } catch (Exception e) {
-            return ResponseEntity.status(500).body(Map.of("error", "could not get players", "message", e.getMessage()));
+            logger.error("Game: {} : Error retrieving all players", gameId, e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "could not get players", "message", e.getMessage()));
         }
     }
 
@@ -122,9 +128,11 @@ public class PlayerController {
     public ResponseEntity<?> getAllPlayers() {
         try {
             List<PlayerModel> allPlayers = playerService.getAllPlayers();
+            logger.info("Successfully retrieved all players");
             return ResponseEntity.ok(allPlayers);
         } catch (Exception e) {
-            return ResponseEntity.status(500).body(Map.of("error", "could not get players", "message", e.getMessage()));
+            logger.error("Error retrieving all players", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("error", "could not get players", "message", e.getMessage()));
         }
     }
 }
